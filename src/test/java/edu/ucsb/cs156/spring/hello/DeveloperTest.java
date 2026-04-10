3e7382dea654c5da8174c3d5041dc3ab3fa60e06
surew1n <sherwin1261@gmail.com>
2026-04-09 17:41:34 -0700

    sr - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -28,4 +28,9 @@ public class DeveloperTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("surew1n", Developer.getGithubId());
+    }
+
 }
