ea3b4613826840191ede339a8f23f7e9db281ac7
surew1n <sherwin1261@gmail.com>
2026-04-09 18:07:29 -0700

    sr - removed TODO comment

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,9 +25,6 @@ public class DeveloperTest {
         assertEquals("Sherwin", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
-
     @Test
     public void getGithubId_returns_correct_githubId() {
         assertEquals("surew1n", Developer.getGithubId());
